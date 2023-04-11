e031af22ca9c47ba109cec0b04c8074eecb3829b
CBL <cyril@dilolabs.fr>
2023-04-11 18:04:17 +0200

    bundle gem

---
+++ lib/devise_crowdsec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "devise_crowdsec/version"
+
+module DeviseCrowdsec
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/devise_crowdsec_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DeviseCrowdsec do
+  it "has a version number" do
+    expect(DeviseCrowdsec::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
